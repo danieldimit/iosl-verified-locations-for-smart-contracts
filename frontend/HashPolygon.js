04d6d74be7a85344bf82e3d5dad2596ad4f06a1c
Radoslav Vlaskovski <radoslav.vlaskovsky@gmail.com>
2017-11-21 09:54:08 +0100

    simple polygon geo-hashing

---
+++ frontend/HashPolygon.js
@@ -0,0 +1,43 @@
+var geohashpoly = require('geohash-poly')
+
+// SET LONGITUDE FIRST THEN LANGITUDE
+// THIS IS AN OCTAGON AROUND BERLIN
+polygon = [[[13.624420, 52.305802],[13.127289, 52.359510],[12.888336, 52.451668],[13.212433, 52.698702],
+   [13.190460, 52.741956], [13.544769, 52.658737],[13.791962, 52.551985],[13.932037, 52.362865], [13.624420, 52.305802]]];
+
+var hashedPolygon = [];
+
+geohashpoly({coords: polygon, precision: 4, hashMode: "extent" }, function (err, hashes) {
+    hashedPolygon = hashes;
+    //console.log(hashedPolygon);
+    var prefix = findCommonPrefix(hashedPolygon);
+    var suffixes = [];
+    for(i = 0; i < hashedPolygon.length; i++){
+        suffixes.push(hashedPolygon[i].substring(prefix.length, hashedPolygon[i].length));
+    }
+    console.log(prefix);
+    console.log(suffixes);
+});
+
+
+function findCommonPrefix(hashes) {
+    prefix = "";
+    minLength = hashes[0].length;
+    for(i = 0; i < hashes.length; i++){
+        if(minLength < hashes[i].length){
+            minLength = hashes[i].length
+        }
+    }
+
+    for (j = 0; j < minLength; j++) {
+        newPrefix = true;
+        for(i = 0; i < hashes.length - 1; i++){
+            if (hashes[i].charAt(j) != hashes[i + 1].charAt(j)) {
+                newPrefix = false;
+            }
+        }
+        if(newPrefix) prefix = hashes[0].substring(0, j+1);
+    }
+
+    return prefix;
+}
--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,17 @@
 # Frontend
 
 Frontend documentation here
+
+
+### Hash Polygons
+
+Requirements:
+
+````bash
+npm install geohash-poly
+````
+
+Simple function that hashes an octagon around Berlin. It returns the data as prefix and suffix,
+so that the list of hashes can be stored efficiently.
+
+More info: https://github.com/derrickpelletier/geohash-poly

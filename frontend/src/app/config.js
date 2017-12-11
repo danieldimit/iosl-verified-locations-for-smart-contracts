b5da4514c41c8be59189a5f3b87f73dc4161500e
danieldimit <dani2dim@yahoo.com>
2017-12-11 04:16:25 +0100

    Fixed docker files to work with the updated project

--- frontend/src/app/config.js
+++ frontend/src/app/config.js
@@ -1,2 +1,2 @@
-export const ethereumBackendUrl = 'http://localhost:4000/api/v1';
+export const ethereumBackendUrl = 'http://192.168.99.100:5000/api/v1';
 export const oracleBackendUrl = 'http://192.168.99.100:4000';

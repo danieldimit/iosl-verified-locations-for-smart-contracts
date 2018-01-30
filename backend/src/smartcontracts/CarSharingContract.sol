96bd4907da6bdb4fe5fd43216cf47e23f0523302
piyasa1991 <piyasa.official@gmail.com>
2018-01-30 10:09:22 +0100

    GetCarDetails method added in smart contract.

--- backend/src/smartcontracts/CarSharingContract.sol
+++ backend/src/smartcontracts/CarSharingContract.sol
@@ -12,15 +12,15 @@ contract CarDetails {
       //string geofence_prefix= "u33";
       //string geofence_prefix;
       //string[] geofence_suffix = ["h", "k","s","u","5","7","e","g","4","6","d","f","1","3","9","c"];
-      int128[] geofenceInt= [855152, 855154, 855160, 855162, 855141, 855143, 855149,855151, 855140, 855142, 855148, 855150, 855137, 855139, 855145, 855147];
-      int128 positionInt = 0;
+      //int128[] geofenceInt= [855152, 855154, 855160, 855162, 855141, 855143, 855149,855151, 855140, 855142, 855148, 855150, 855137, 855139, 855145, 855147];
+      //int128 positionInt = 0;
       //address constant oracle = 0x8ead2d9305536ebdde184cea020063d2de3665c7;
       address oracle;
       uint pendingWithdrawals = 0;
       
-      bytes32 position;
-      bytes8 geofence_prefix;
-      bytes4[] geofence_suffix;
+      bytes16 position;
+      bytes6 geofence_prefix;
+      bytes16[] geofence_suffix;
       
       modifier onlyOwner() {
         require(msg.sender == owner);
@@ -34,14 +34,12 @@ contract CarDetails {
 
       //event TraceLocation(bytes16 number);
       event CarRenterSatus(address renter, address car , bool status);
-      
-      
 
       /////////////////////////////////////
       //            Functions 
       /////////////////////////////////////
       
-      function CarDetails(bytes16 _carGSMNum, uint _penaltyValue, bytes32 _position, bytes8 _geofencePrefix, bytes4[] _geofenceSuffix)  {
+      function CarDetails(bytes16 _carGSMNum, uint _penaltyValue, bytes16 _position, bytes6 _geofencePrefix, bytes16[] _geofenceSuffix)  {
         carGSMNum = _carGSMNum;
         owner= msg.sender;
         penaltyValue = _penaltyValue;
@@ -51,7 +49,7 @@ contract CarDetails {
       
     
       //Check if "curPos" hash is within the geofence defined in the "geofence" array 
-      function checkPositionInGeofenceGeohash() {
+      //function checkPositionInGeofenceGeohash() {
           //bytes memory bPrefixFence = bytes(geofence_prefix);
           //bytes memory bPosition = bytes(position);
           //for(uint i; i<3; i++){
@@ -78,45 +76,84 @@ contract CarDetails {
                   
               //} 
               
-        for(uint i;i<3;i++){
-              if(geofence_prefix[i]!=position[i]){
-                  leftGeofence = true;
-                  break;
-              }
-          }
-          if(leftGeofence==false){
-                 leftGeofence=true;
-              for(uint j; j<geofence_suffix.length;j++){
-                  bytes4 temp = geofence_suffix[j];
-                  if(bytes1(position[3])==bytes1(temp[0]))
-                  {
-                      leftGeofence = false;
-                      break;
-                  }
-              }
-          }
-        }
-
-      
-      function checkPositionInGeofenceIntGeoHash() {
+        ///for(uint i;i<3;i++){
+             // if(geofence_prefix[i]!=position[i]){
+                  ///leftGeofence = true;
+                 // break;
+             // }
+         // }
+         // if(leftGeofence==false){
+               //  leftGeofence=true;
+            //  for(uint j; j<geofence_suffix.length;j++){
+                //  bytes4 temp = geofence_suffix[j];
+                //  if(bytes1(position[3])==bytes1(temp[0]))
+                 // {
+                  //    leftGeofence = false;
+                 //     break;
+                 // }
+            //  }
+        //  }
+      //  }
+     // function checkPositionInGeofenceIntGeoHash() {
         
         ///The position and the geofence have to be encoded with the same amount of bits
         //If for example the position is encoded with more bits, it has to be shifted
         //to the same amount of bits that the geofence is encoded with
         //
-        bool inside = false;
-        for(uint i; i < geofenceInt.length; i++){
-            if(positionInt == geofenceInt[i]){
-                inside = true;
-            }
-        }
-        leftGeofence = !inside;
+       // bool inside = false;
+       // for(uint i; i < geofenceInt.length; i++){
+       //     if(positionInt == geofenceInt[i]){
+       //         inside = true;
+       //     }
+       // }
+       // leftGeofence = !inside;
+     // }
+     
+        function CheckPositionInGeofence(bytes16 position, bytes6 prefix, bytes16[] suffix) 
+      public constant returns (bool)
+      {   
+          bool out = leftGeofence;
+          
+          //violation condition for prefix
+            if(position[0]!=prefix[0] || position[1]!=prefix[1] || position[2]!=prefix[2]){
+                out=true;
+          }
+          else if(out==false)
+          {
+              for(uint i = 0; i<suffix.length;i++){
+                  bytes16 hash = suffix[i];
+                  for(uint j=0;j<hash.length;j++){
+                      //if the bytes do not match
+                      if(position[j+3]!=hash[j]){
+                out=true;
+                // do not need to check for the current geofence value
+                //move to the next suffix
+                break; 
+                  }
+              //if they match
+              else{
+                  out=false;
+                  //continue; // keep checking the next character
+              }
+                //since the geofences suffix may be of variable length
+              // stop checking if two consecutive positions are 0
+              if(hash[j+1]==0 && hash[j+2]==0){
+                  break;//end of string and do not check further
+              }
+          
+                  }
+                      if(out==false){
+                          break;
+                   }
+              }
+          }
+          leftGeofence=out;
+          return leftGeofence;
       }
 
       /////////////////////////////////////
       // Functions called by car itself 
       /////////////////////////////////////
-    
     
       function isAvailable() public constant returns (bool) {
         return availability;
@@ -131,16 +168,6 @@ contract CarDetails {
         
         selfdestruct(owner);
       }
-    
-      /*
-      function MonitorCarLocation(bytes16 _carGSMNum){
-            // TODO: trigger oracle event
-            TraceLocation(_carGSMNum);
-            availability=false;
-            position ="u33a";
-            checkPositionInGeofenceGeohash();
-      }
-      */
 
       function SetCarStatus(address renter,bool status){
             //TraceLocation(_carGSMNum);
@@ -152,13 +179,22 @@ contract CarDetails {
           oracle=_oracle;
       }
       
+      function GetCarDetails() onlyOwner public constant returns(uint _penaltyValue,bytes16 _carGSMNum, 
+      bytes8 _geofencePrefix, bytes4[] _geofenceSuffix) {
+          _penaltyValue = penaltyValue;
+          _carGSMNum = carGSMNum;
+          _geofencePrefix = geofence_prefix;
+          _geofenceSuffix=geofence_suffix;
+      }
+      
       /////////////////////////////////////
       // Functions called by the oracle 
       /////////////////////////////////////
 
-      function updatePosition(bytes32 curPos) onlyOracle {
+      function updatePosition(bytes16 curPos) onlyOracle {
             position = curPos;
-            checkPositionInGeofenceGeohash();
+           // checkPositionInGeofenceGeohash();
+           CheckPositionInGeofence(curPos,geofence_prefix,geofence_suffix);
       }
 
       function getPosition() constant returns (bytes32) {

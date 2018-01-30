cdbc3e2ec2ea0381b4dc2fd587e67c8debb77717
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2018-01-30 12:25:12 +0100

    improve cell distribution plot

--- oracle/hashing/visualization/plot_cell_distribution/plot_cell_distribution.py
+++ oracle/hashing/visualization/plot_cell_distribution/plot_cell_distribution.py
@@ -6,12 +6,14 @@ def read():
 
         u10 = []
         o10 = []
+        o15 = []
         o20 = []
 
         while line:
 
             u_10 = 0.0
             o_10 = 0.0
+            o_15 = 0.0
             o_20 = 0.0
 
             x = line.rstrip()
@@ -24,18 +26,21 @@ def read():
                 value = float(kp[1])
                 if key < 10:
                     u_10 += value
-                elif key < 20:
+                elif key < 15:
                     o_10 += value
+                elif key < 20:
+                    o_15 += value
                 else:
                     o_20 += value
 
             u10.append(u_10)
             o10.append(o_10)
+            o15.append(o_15)
             o20.append(o_20)
 
             line = fp.readline()
 
-        return u10, o10, o20
+        return u10, o10, o15, o20
 
 
 if __name__ == "__main__":
@@ -48,10 +53,12 @@ if __name__ == "__main__":
     ax1 = plt.subplot(221)
     ax2 = plt.subplot(222)
     ax3 = plt.subplot(223)
+    ax4 = plt.subplot(224)
 
     ax1.bar(label, info[0], width, color="b", label="Preicision <10")
     ax2.bar(label, info[1], width, color="orange", label="Precision >=10 and <20")
-    ax3.bar(label, info[2], width, color="r", label="Preicsion >20")
+    ax3.bar(label, info[2], width, color="green", label="Preicsion >=15 < 20")
+    ax4.bar(label, info[3], width, color="r", label="Preicsion >20")
     ax1.legend(loc='best')
     ax2.legend(loc='best')
     ax3.legend(loc='best')
